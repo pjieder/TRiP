2330883ae214216a8feb4b71aabd813829ff7ebf
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-04-20 21:41:38 +0200

    Email regex
    Added a regex for checking correct email is entered. Also made it possible to only select one role when registering new worker.

--- TRiP/src/trip/gui/TRiP.java
+++ TRiP/src/trip/gui/TRiP.java
@@ -21,7 +21,7 @@ public class TRiP extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/RegisterForm.fxml"));
       
         Scene scene = new Scene(root);
         
--- TRiP/src/trip/gui/controllers/RegisterFormController.java
+++ TRiP/src/trip/gui/controllers/RegisterFormController.java
@@ -5,8 +5,14 @@
  */
 package trip.gui.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RegexValidator;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -16,12 +22,50 @@ import javafx.fxml.Initializable;
  */
 public class RegisterFormController implements Initializable {
 
+    @FXML
+    private JFXCheckBox adminCheckbox;
+    @FXML
+    private JFXCheckBox userCheckbox;
+    @FXML
+    private JFXTextField testEmailTextField;
+    @FXML
+    private JFXButton registerButton;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        
+        RegexValidator regex = new RegexValidator();
+        regex.setRegexPattern("^.+@[^\\.].*\\.[a-z]{2,}$");
+        regex.setMessage("Input is not a valid email");
+        testEmailTextField.getValidators().add(regex);
+        testEmailTextField.textProperty().addListener((observable, oldValue, newValue)->{
+            registerButton.setDisable(!testEmailTextField.validate());
+        });
+    }
+
+    @FXML
+    private void doAdmin(ActionEvent event) {
+
+        if (adminCheckbox.isSelected()) {
+            userCheckbox.setSelected(false);
+        } else {
+            userCheckbox.setSelected(true);
+        }
+
+    }
+
+    @FXML
+    private void doUser(ActionEvent event) {
+
+        if (userCheckbox.isSelected()) {
+            adminCheckbox.setSelected(false);
+        } else {
+            adminCheckbox.setSelected(true);
+        }
+
+    }
+
 }
