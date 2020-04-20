c7e92c693006ea3a6e10fcd132f624ec211eb6b2
pjieder <55383141+pjieder@users.noreply.github.com>
2020-04-20 14:07:03 +0200

    login view

--- TRiP/src/trip/TRiP.java
+++ TRiP/src/trip/TRiP.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,12 +20,15 @@ public class TRiP extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("TRiP");
+        stage.getIcons().add(new Image(TRiP.class.getResourceAsStream("time.png")));
+        stage.setResizable(false);
     }
 
     /**
