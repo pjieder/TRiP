b80145bc87c3e854efc45cf853bb1e03a47fa949
pjieder <55383141+pjieder@users.noreply.github.com>
2020-04-22 18:06:51 +0200

    view updates

---
+++ TRiP/src/trip/gui/controllers/AdminCurrentUserViewController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trip.gui.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Peter
+ */
+public class AdminCurrentUserViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
