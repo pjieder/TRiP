d2bcfca4bbc0c2facbc1c91a0b51284594a7e611
pjieder <55383141+pjieder@users.noreply.github.com>
2020-04-26 15:13:29 +0200

    -||-

--- TRiP/src/trip/gui/controllers/MenuBarViewController.java
+++ TRiP/src/trip/gui/controllers/MenuBarViewController.java
@@ -5,9 +5,18 @@
  */
 package trip.gui.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
@@ -16,6 +25,11 @@ import javafx.fxml.Initializable;
  */
 public class MenuBarViewController implements Initializable {
 
+    @FXML
+    private BorderPane BP;
+    @FXML
+    private AnchorPane AP;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +37,45 @@ public class MenuBarViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void openProjectsView(MouseEvent event) {
+        BP.setCenter(AP);
+    }
+
+    @FXML
+    private void openUserView(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void openStatisticsView(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void openTimeTrackingView(MouseEvent event) {
+
+    }
+
+    @FXML
+    private void openEconomyView(MouseEvent event) {
+    }
+
+    @FXML
+    private void logOut(MouseEvent event) {
+    }
+    
+    private void loadPage(String page) {
+        Parent root = null;
+        
+        try {
+            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(MenuBarViewController.class.getName()).log(Level.SEVERE, null, ex);
+        BP.setCenter(root);
+    }
+    }
+           
     
 }
