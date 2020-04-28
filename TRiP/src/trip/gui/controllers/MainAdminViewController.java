3e4a6e34f31a0898049ddaaa3356e035437e5ff7
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-04-28 11:02:44 +0200

    Added delete functionality with Nikolaj

--- TRiP/src/trip/gui/controllers/MainAdminViewController.java
+++ TRiP/src/trip/gui/controllers/MainAdminViewController.java
@@ -5,8 +5,10 @@
  */
 package trip.gui.controllers;
 
+import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
@@ -14,14 +16,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import trip.be.Project;
 import trip.gui.AppModel;
 import trip.gui.models.ProjectModel;
@@ -54,6 +58,8 @@ public class MainAdminViewController implements Initializable {
     private ImageView activeArrow;
     @FXML
     private ImageView inactiveArrow;
+    @FXML
+    private JFXButton deleteButton;
 
     /**
      * Initializes the controller class.
@@ -168,17 +174,42 @@ public class MainAdminViewController implements Initializable {
         activeProjects.setVisible(true);
         activeArrow.setVisible(false);
         inactiveArrow.setVisible(true);
+        deleteButton.setVisible(true);
         projectTable.setItems(projectModel.loadAllInactiveProjects());
         projectTable.refresh();
     }
 
     @FXML
-    private void showIActiveProjects(MouseEvent event) {
+    private void showActiveProjects(MouseEvent event) {
         inactiveProjects.setVisible(true);
         activeProjects.setVisible(false);
         activeArrow.setVisible(true);
         inactiveArrow.setVisible(false);
+        deleteButton.setVisible(false);
         projectTable.setItems(projectModel.loadAllActiveProjects());
         projectTable.refresh();
+    }
+
+    @FXML
+    private void deleteProject(ActionEvent event) {
+
+        if (!projectTable.getSelectionModel().isEmpty()) {
+            
+            Project selectedProject = projectTable.getSelectionModel().getSelectedItem();
+            
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.initStyle(StageStyle.UTILITY);
+            alert.setTitle("Confirm delete");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to delete this project? All logged time will no longer be accessable. Proceed?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                projectTable.getItems().remove(selectedProject);
+                projectModel.deleteProject(selectedProject);
+            } else {
+            }
+
+        }
+
     }
 }
