4625543a7a74adcacac2e5444b22007978f6c523
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-05-03 17:05:05 +0200

    Removed class that was not needed & added javadoc for utility classes.

--- TRiP/src/trip/utilities/TimeConverter.java
+++ TRiP/src/trip/utilities/TimeConverter.java
@@ -20,6 +20,11 @@ import javafx.util.Duration;
  */
 public class TimeConverter {
 
+    /**
+     * Converts an amount of seconds to a string representing the time.
+     * @param seconds The seconds to be represented as a string.
+     * @return A string representing the amount of time in a HH:mm:ss format.
+     */
     public static String convertSecondsToString(int seconds) {
         Duration duration = Duration.seconds(seconds);
         String timeString = String.format("%02d:%02d:%02d",
@@ -28,12 +33,36 @@ public class TimeConverter {
                 (int) duration.toSeconds() % 60);
         return timeString;
     }
+    
+    /**
+     * Converts a string value representing time to an int value containing the total amount of time in seconds.
+     * @param string The string representing the amount of time in a HH:mm:ss format.
+     * @return 
+     */
+    public static int convertStringToSeconds(String string) {
+        String[] time = string.split(":");
+        int hours = Integer.parseInt(time[0]);
+        int minutes = Integer.parseInt(time[1]);
+        int seconds = Integer.parseInt(time[2]);
 
+        return (hours * 60 * 60) + (minutes * 60) + seconds;
+    }
+
+    /**
+     * Converts a date object to a string based on the format dd-MM-yyyy HH:mm
+     * @param date The date to be converted
+     * @return A string representing the date.
+     */
     public static String convertDateToString(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
         return simpleDateFormat.format(date);
     }
 
+    /**
+     * Converts a string to a date object based on the format dd-MM-yyyy HH:mm
+     * @param dateString The string to be converted
+     * @return A date object of the entered date.
+     */
     public static Date convertStringToDate(String dateString) {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
@@ -44,11 +73,22 @@ public class TimeConverter {
         return null;
     }
 
+    /**
+     * Converts a date object to a string based on the format yyyy-MM-dd HH:mm for easier sorting in
+     * the database.
+     * @param date The date to be converted.
+     * @return A string representing the date..
+     */
     public static String convertDateToStringDB(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         return simpleDateFormat.format(date);
     }
 
+    /**
+     * Converts a string to a date object based on the format yyyy-MM-dd HH:mm.
+     * @param dateString The string to be converted.
+     * @return A date object of the entered date.
+     */
     public static Date convertStringToDateDB(String dateString) {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
@@ -59,24 +99,22 @@ public class TimeConverter {
         return null;
     }
 
-    public static String convertLocalDateToStringDB(LocalDate date) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        return simpleDateFormat.format(date);
-    }
-    
-    public static int convertStringToSeconds(String string) {
-        String[] time = string.split(":");
-        int hours = Integer.parseInt(time[0]);
-        int minutes = Integer.parseInt(time[1]);
-        int seconds = Integer.parseInt(time[2]);
-
-        return (hours * 60 * 60) + (minutes * 60) + seconds;
-    }
-
+    /**
+     * Adds the amount of to the localdate object.
+     * @param date The date that the calculation is based upon.
+     * @param days The amount of days to be added (or subtracted).
+     * @return 
+     */
     public static LocalDate addDays(LocalDate date, int days) {
         return date.plusDays(days);
     }
 
+    /**
+     * Returns a list containing all the days between and indcluding the entered days.
+     * @param date1 The first day.
+     * @param date2 The last day.
+     * @return A list containing all days between and uncluding the entered days.
+     */
     public static List<LocalDate> getDaysBetweenDates(LocalDate date1, LocalDate date2) {
         
         List<LocalDate> datesBetween = new ArrayList();
