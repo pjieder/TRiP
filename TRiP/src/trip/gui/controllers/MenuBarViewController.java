c230552fc4b455a0651a3852da55ea70a7f79b53
Anders Elis Præstegaard Madsen <ande530c@easv365.dk>
2020-05-26 15:05:06 +0200

    Added CSS for context menu

--- TRiP/src/trip/gui/controllers/MenuBarViewController.java
+++ TRiP/src/trip/gui/controllers/MenuBarViewController.java
@@ -49,6 +49,7 @@ public class MenuBarViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Platform.runLater(()->{AP.getScene().getStylesheets().add(TRiP.class.getResource("styles/ContextCSS.css").toExternalForm());});
     }
 
     /**
